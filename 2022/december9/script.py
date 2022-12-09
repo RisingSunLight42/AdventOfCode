214ce30e0819e4e8fa54d3067ae71ab18ddbb8aa
RisingSunLight <risingsunlight5776@gmail.com>
2022-12-09 23:29:26 +0100

    🐛 Edit script to be more concise

--- 2022/december9/script.py
+++ 2022/december9/script.py
@@ -1,3 +1,4 @@
+import datetime
 instructions_list = []
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
@@ -25,29 +26,12 @@ def is_touching(coord_H, coord_T):
 
 def move_tail(coord_H, coord_T):
     if (coord_H[0] == coord_T[0]):
-        if (coord_H[1] > coord_T[1]):
-            coord_T[1] += 1
-        else:
-            coord_T[1] -= 1
+        coord_T[1] += 1 + (-2 * int(coord_H[1] < coord_T[1]))
     elif (coord_H[1] == coord_T[1]):
-        if (coord_H[0] > coord_T[0]):
-            coord_T[0] += 1
-        else:
-            coord_T[0] -= 1
-    elif (coord_H[0] > coord_T[0]):
-        if (coord_H[1] > coord_T[1]):
-            coord_T[0] += 1
-            coord_T[1] += 1
-        else:
-            coord_T[0] += 1
-            coord_T[1] -= 1
-    elif (coord_H[0] < coord_T[0]):
-        if (coord_H[1] > coord_T[1]):
-            coord_T[0] -= 1
-            coord_T[1] += 1
-        else:
-            coord_T[0] -= 1
-            coord_T[1] -= 1
+        coord_T[0] += 1 + (-2 * int(coord_H[0] < coord_T[0]))
+    else:
+        coord_T[0] += 1 + (-2 * int(coord_H[0] < coord_T[0]))
+        coord_T[1] += 1 + (-2 * int(coord_H[1] < coord_T[1]))
     return coord_T
 
 
@@ -56,7 +40,6 @@ def execute_tail(coord_H, coord_T, pos):
         coord_T = move_tail(coord_H, coord_T)
         if (pos == 9):
             bridge_visited[coord_T[0]][coord_T[1]] = '#'
-        return coord_T
     return coord_T
 
 
@@ -64,42 +47,26 @@ def check_how_much_visited():
     visited = 0
     for i in range(len(bridge_visited)):
         for j in range(len(bridge_visited[i])):
-            if bridge_visited[i][j] == '#':
-                visited += 1
+            visited += bridge_visited[i][j] == '#'
     return visited
 
 
 def part_one():
+    start_time = datetime.datetime.now()
     all_tails = []
     for _ in range(10):
         all_tails.append([499, 0])
     for instruction in instructions_list:
         movement = instruction.split(" ")[0]
         count = int(instruction.split(" ")[1])
-        if movement == 'U':
-            for _ in range(count):
-                all_tails[0][0] -= 1
-                for j in range(9):
-                    all_tails[j +
-                              1] = execute_tail(all_tails[j], all_tails[j + 1], j + 1)
-        elif movement == 'R':
-            for _ in range(count):
-                all_tails[0][1] += 1
-                for j in range(9):
-                    all_tails[j +
-                              1] = execute_tail(all_tails[j], all_tails[j + 1], j + 1)
-        elif movement == 'D':
-            for _ in range(count):
-                all_tails[0][0] += 1
-                for j in range(9):
-                    all_tails[j +
-                              1] = execute_tail(all_tails[j], all_tails[j + 1], j + 1)
-        elif movement == 'L':
-            for _ in range(count):
-                all_tails[0][1] -= 1
-                for j in range(9):
-                    all_tails[j +
-                              1] = execute_tail(all_tails[j], all_tails[j + 1], j + 1)
+        for _ in range(count):
+            all_tails[0][movement in ['L', 'R']] += 1 + \
+                (-2 * int(movement in ['L', 'U']))
+            for j in range(9):
+                k = j + 1
+                all_tails[k] = execute_tail(all_tails[j], all_tails[k], k)
+    end_time = datetime.datetime.now()
+    print(end_time-start_time)
     print(check_how_much_visited())
 
 
