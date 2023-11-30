92aaa753a1e015f534a74da3e294bc94a3967d03
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-01 00:48:18 +0100

    ➕ Add setup for day one of december 2023

---
+++ 2023/december1/script.py
@@ -0,0 +1,18 @@
+list_test = []
+with open("./input.txt", 'r', encoding='utf-8') as file:
+    list_test = file.readlines()
+
+# Part one
+
+
+def part_one():
+    print("hello world")
+
+# Part two
+
+
+def part_two():
+    print("hello world")
+
+
+part_one()
