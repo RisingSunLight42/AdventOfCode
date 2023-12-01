7377bf30003150fc5883ccf7296658e35c097a71
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-01 16:52:47 +0100

    ➕ add day 1 of AoC 2023

--- 2023/december1/script.py
+++ 2023/december1/script.py
@@ -1,18 +1,35 @@
-list_test = []
+import re
+
+lines = []
 with open("./input.txt", 'r', encoding='utf-8') as file:
-    list_test = file.readlines()
+    lines = file.readlines()
 
 # Part one
 
 
 def part_one():
-    print("hello world")
+    total = 0
+    for line in lines:
+        all_digit_matches = re.findall("\d", line)
+        total += int(all_digit_matches[0] + all_digit_matches[-1])
+    print(total)
+        
 
 # Part two
 
+match_dict = { "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
 def part_two():
-    print("hello world")
+    total = 0
+    for line in lines:
+        all_matches = re.findall(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))', line)
+        first = all_matches[0]
+        last = all_matches[-1]
+        first = first if first.isdigit() else match_dict[first]
+        last = last if last.isdigit() else match_dict[last]
+        total += int(first + last)
+    print(total)
+    
 
+part_two()
 
-part_one()
