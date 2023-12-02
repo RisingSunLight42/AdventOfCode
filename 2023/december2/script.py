54e99859bb5ba4e33d8c2650b9463d60fdd30ee1
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-02 06:40:13 +0100

    ♻️ rename variables and remove useless part of code

--- 2023/december2/script.py
+++ 2023/december2/script.py
@@ -14,9 +14,9 @@ def part_one(red=12, green=13, blue=14):
         dict_actual_game = { "red": 0, "blue": 0, "green": 0 }
         game_list = line.split(":")
         game_id = int(game_list[0].split(" ")[1])
-        cube_set = game_list[1]
-        cube_sets = cube_set.split(";")
-        for cubes in sets:
+        cube_set_string = game_list[1]
+        cube_sets = cube_set_string.split(";")
+        for cubes in cube_sets:
             number_and_cube = cubes.split(",")
             for number_and_cube in number_and_cube_list:
                 [number, color] = number_and_cube.strip().split(" ")
@@ -36,25 +36,20 @@ def part_one(red=12, green=13, blue=14):
 
 def part_two():
     total = 0
-    fine_list = []
     for line in lines:
         dict_actual_game = { "red": 0, "blue": 0, "green": 0 }
         game_list = line.split(":")
         game_id = int(game_list[0].split(" ")[1])
-        colors = game_list[1]
-        tries = colors.split(";")
-        for trying in tries:
-            list_of_colors = trying.split(",")
-            for color_number in list_of_colors:
-                [number, color] = color_number.strip().split(" ")
+        cube_set_string = game_list[1]
+        cube_sets = cube_set_string.split(";")
+        for cubes in cube_sets:
+            number_and_cube_list = cubes.split(",")
+            for number_and_cube in number_and_cube_list:
+                [number, color] = number_and_cube.strip().split(" ")
                 number = int(number)
                 if (dict_actual_game[color] < number):
                     dict_actual_game[color] = number
-        fine_list.append(dict_actual_game["green"] * dict_actual_game["red"] * dict_actual_game["blue"])
-    
-    for element in fine_list:
-        total += element
-
+        total += dict_actual_game["green"] * dict_actual_game["red"] * dict_actual_game["blue"]
     print(total)
     
 
