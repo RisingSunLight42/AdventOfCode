2b9e4a4eb1109672f4f4f8ade2d8db98fdf37966
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-03 05:53:21 +0100

    ➕ Add second part of AoC dec. 3 2021

--- 2021/december3/script.py
+++ 2021/december3/script.py
@@ -24,5 +24,35 @@ def first_part():
             espilon += '1'
     print(int(gamma, 2) * int(espilon, 2))
 
-
-first_part()
+def second_part():
+    oxygen_gen = binaries_list.copy()
+    c02_rating = binaries_list.copy()
+    iterator = 0
+    for i in range(len(binaries_list[0]) - 1):
+        count_oxygen = [0, 0]
+        count_c02 = [0, 0]
+        for binary in oxygen_gen:
+            if binary[iterator] == '0':
+                count_oxygen[0] += 1
+            else:
+                count_oxygen[1] += 1
+        for binary in c02_rating:
+            if binary[iterator] == '0':
+                count_c02[0] += 1
+            else:
+                count_c02[1] += 1
+        value_filter_oxygen = '1'
+        value_filter_co0 = '0'
+        if count_oxygen[0] > count_oxygen[1]:
+            value_filter_oxygen = '0'
+        if count_c02[0] > count_c02[1]:
+            value_filter_co0 = '1'
+        if len(oxygen_gen) != 1:
+            oxygen_gen = list(filter(lambda given_item: given_item[iterator] == value_filter_oxygen, oxygen_gen))
+        if len(c02_rating) != 1:
+            c02_rating = list(filter(lambda given_item: given_item[iterator] == value_filter_co0, c02_rating))
+        iterator += 1
+    print(str(int(oxygen_gen[0].replace("\n", ""), 2)) + " " + str(int(c02_rating[0].replace("\n", ""), 2)))
+    print(int(oxygen_gen[0].replace("\n", ""), 2) * int(c02_rating[0].replace("\n", ""), 2))
+    
+second_part()
