5a928759b0e926fc5c8a98a92f9d0725c685c310
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-03 05:54:17 +0100

    ➕ Add draft for dec. 3 2023

---
+++ 2023/december3/script.py
@@ -0,0 +1,22 @@
+import re
+
+lines = []
+with open("./input.txt", 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+
+# Part one
+def part_one():
+    total = 0
+
+    print(total)
+        
+
+# Part two
+def part_two():
+    total = 0
+    
+    print(total)
+    
+
+part_one()
