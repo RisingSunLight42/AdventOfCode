13219a31b2672436ba0b68ad30200960cdbc1e49
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-04 23:59:40 +0100

    ➕ Add preparations for day 5 of 2023

---
+++ 2023/december5/script.py
@@ -0,0 +1,15 @@
+import re
+
+lines = []
+with open("./input.txt", 'r', encoding='utf-8') as file:
+    lines = file.read().splitlines()  
+
+
+def part_one():
+    print("a")
+
+
+def part_two():
+    print("a")
+
+part_one()
