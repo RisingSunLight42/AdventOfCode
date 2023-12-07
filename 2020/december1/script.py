e1a84b82ef2f9a746d249425ac636f31190a7f21
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-07 22:53:11 +0100

    ➕ Add day 1 for 2020

--- 2020/december1/script.py
+++ 2020/december1/script.py
@@ -1,15 +1,24 @@
 import re
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
-    lines = file.read().splitlines()
+    entries = [int(line) for line in file.read().splitlines()]
 
 
 def part_one():
-    pass
+    for i in range(len(entries)):
+        for j in range(i+1, len(entries)):
+            if entries[i]+entries[j]==2020:
+                print(entries[i] * entries[j])
+                return
 
 
 def part_two():
-    pass
+    for i in range(len(entries)):
+        for j in range(i+1, len(entries)):
+            for k in range(i+2, len(entries)):
+                if entries[i]+entries[j]+entries[k]==2020:
+                    print(entries[i] * entries[j] * entries[k])
+                    return
 
 part_one()
 part_two()
