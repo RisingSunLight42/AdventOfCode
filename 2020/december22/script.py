82fc4a8aaffc342555753be98f3ce84ed59b157b
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-07 19:16:34 +0100

    🔨 Add all folders for every days of every years

---
+++ 2020/december22/script.py
@@ -0,0 +1,15 @@
+import re
+
+with open("./input.txt", 'r', encoding='utf-8') as file:
+    lines = file.read().splitlines()
+
+
+def part_one():
+    pass
+
+
+def part_two():
+    pass
+
+part_one()
+part_two()
