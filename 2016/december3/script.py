1d65af3b5d9ad7fec7247f47c08bada6498cedf1
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-08 00:12:06 +0100

    ➕ add day 3 for 2016

--- 2016/december3/script.py
+++ 2016/december3/script.py
@@ -1,15 +1,33 @@
 import re
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
-    lines = file.read().splitlines()
+    triangles = [list(map(int, re.findall(r"\d+", line))) for line in file.read().splitlines()]
 
 
 def part_one():
-    pass
+    valid = 0
+    for triangle in triangles:
+        two_smallest_side = []
+        for i in range(2):
+            mini = min(triangle)
+            triangle.remove(mini)
+            two_smallest_side.append(mini)
+        valid += sum(two_smallest_side) > triangle[0]
+    print(valid)
 
 
 def part_two():
-    pass
+    valid = 0
+    for i in range(len(triangles[0])):
+        for j in range(0, len(triangles) - 2, 3):
+            two_smallest_side = []
+            triangle = [triangles[j][i], triangles[j+1][i], triangles[j+2][i]]
+            for k in range(2):
+                mini = min(triangle)
+                triangle.remove(mini)
+                two_smallest_side.append(mini)
+            valid += sum(two_smallest_side) > triangle[0]
+    print(valid)
 
-part_one()
+# part_one()
 part_two()
