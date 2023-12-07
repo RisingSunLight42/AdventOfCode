84c3ce00794880a753e23bbb3ac46bc649818267
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-07 22:17:40 +0100

    ➕ add day 1 for 2018

--- 2018/december1/script.py
+++ 2018/december1/script.py
@@ -1,15 +1,27 @@
 import re
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
-    lines = file.read().splitlines()
+    frequencies = [int(re.findall(r".\d+", line)[0]) for line in file.read().splitlines()]
 
 
 def part_one():
-    pass
+    print(sum(frequencies))
 
 
 def part_two():
-    pass
+    iterator = 0
+    is_found = False
+    frequency = 0
+    frequencies_seen = set()
+    frequencies_seen.add(frequency)
+    while not is_found:
+        frequency += frequencies[iterator]
+        if frequency in frequencies_seen:
+            is_found = True
+        else:
+            frequencies_seen.add(frequency)
+        iterator = (iterator + 1) % len(frequencies)
+    print(frequency)
 
 part_one()
 part_two()
