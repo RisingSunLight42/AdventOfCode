ca708235b6eff8aed9beb47f43ee045222aa2a28
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-07 22:27:08 +0100

    ➕ Add day 1 of 2019

--- 2019/december1/script.py
+++ 2019/december1/script.py
@@ -1,15 +1,23 @@
-import re
+from math import floor
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
-    lines = file.read().splitlines()
+    mass_list = [int(line) for line in file.read().splitlines()]
 
 
 def part_one():
-    pass
+    print(sum([floor(mass/3)-2 for mass in mass_list]))
 
 
 def part_two():
-    pass
+    total = 0
+    for mass in mass_list:
+        calc = subtotal = floor(mass/3)-2
+        while calc != 0:
+            calc = floor(calc/3)-2
+            calc = 0 if calc < 0 else calc
+            subtotal += calc
+        total += subtotal
+    print(total)
 
 part_one()
 part_two()
