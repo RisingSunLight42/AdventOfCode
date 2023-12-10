3994d9f36eb23af23cf316a63c1a9e2aaec9f9bc
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-10 23:30:49 +0100

    ➕ add day 5 for 2016

--- 2016/december05/script.py
+++ 2016/december05/script.py
@@ -1,15 +1,32 @@
-import re
+from hashlib import md5
 
-with open("./input.txt", 'r', encoding='utf-8') as file:
-    lines = file.read().splitlines()
+input_value = "ffykfhsq"
 
-
-def part_one():
-    pass
+def part_one(input_value=input_value):
+    nb = 0
+    str_result = ''
+    for i in range(100_000_000_000):
+        hashedValue = md5((input_value + str(i)).encode()).hexdigest()
+        if hashedValue[:5] == '00000':
+            str_result += hashedValue[5]
+            nb += 1
+            if nb == 8:
+                break
+    print(str_result)
 
 
 def part_two():
-    pass
+    nb = 0
+    str_result = [-1,-1,-1,-1,-1,-1,-1,-1]
+    for i in range(100_000_000_000_000):
+        hashedValue = md5((input_value + str(i)).encode()).hexdigest()
+        if hashedValue[:5] == '00000':
+            if hashedValue[5].isdigit() and int(hashedValue[5]) < 8 and str_result[int(hashedValue[5])] == -1:
+                str_result[int(hashedValue[5])] = hashedValue[6]
+                nb += 1
+                if nb == 8:
+                    break
+    print(''.join(str_result))
 
-part_one()
+#part_one()
 part_two()
