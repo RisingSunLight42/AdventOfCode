03c6fd36168ed383cc1dd176c578b5f5a6d183d1
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-11 00:17:20 +0100

    ➕ Add day 7 for 2016

--- 2016/december07/script.py
+++ 2016/december07/script.py
@@ -1,15 +1,33 @@
-import re
+import regex as re
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
     lines = file.read().splitlines()
 
 
 def part_one():
-    pass
+    regex_abba = re.compile(r'([a-z])((?!\1)[a-z])\2\1')
+    total = 0
+    for line in lines:
+        supernet = re.sub(r'\[[a-z]+\]', '-', line)
+        hypernet = '-'.join(re.findall(r'\[[a-z]+\]', line))
+
+        if regex_abba.search(supernet) and not regex_abba.search(hypernet):
+            total += 1
+    print(total)
 
 
 def part_two():
-    pass
+    re_aba = re.compile(r'([a-z])((?!\1)[a-z])\1')
+    total = 0
+    for line in lines:
+        supernet = re.sub(r'\[[a-z]+\]', '-', line)
+        hypernet = '-'.join(re.findall(r'\[[a-z]+\]', line))
+    
+        for (a, b) in re_aba.findall(supernet, overlapped = True):
+            if b + a + b in hypernet:
+                total += 1
+                break
+    print(total)
 
 part_one()
 part_two()
