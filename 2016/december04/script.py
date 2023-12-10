b96c6764304d5d963f22c814d81b87d65002f285
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-10 23:12:12 +0100

    ➕ please kill me i'm too tired, it makes me dumb, add day 4 of 2016

--- 2016/december04/script.py
+++ 2016/december04/script.py
@@ -1,15 +1,57 @@
 import re
+from collections import Counter
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
-    lines = file.read().splitlines()
+    rooms = [ [re.sub(r"-\d+",'',line[0]), int(re.findall(r"\d+",line[0])[0]), line[1][:-1]]for line in [line.split("[") for line in file.read().splitlines()]]
 
-
+# too high
 def part_one():
-    pass
+    total = 0
+    passing_element = []
+    for room in rooms:
+        encrypt, sector, checksum = room
+        counter = Counter(encrypt)
+        counter = counter.most_common()
+        counter = list(filter(lambda a: a[0] in checksum, counter))
+        if len(counter) < 5:
+            continue
+        char_check = ''
+        for letter, number in list(sorted(counter, key=lambda a: (-a[1], a[0]))):
+            char_check += letter
+        if char_check == checksum:
+            passing_element.append(encrypt)
+            total += sector
+    print(total)
+
 
 
 def part_two():
-    pass
+    total = 0
+    passing_element = []
+    for room in rooms:
+        encrypt, sector, checksum = room
+        counter = Counter(encrypt)
+        counter = counter.most_common()
+        counter = list(filter(lambda a: a[0] in checksum, counter))
+        if len(counter) < 5:
+            continue
+        char_check = ''
+        for letter, number in list(sorted(counter, key=lambda a: (-a[1], a[0]))):
+            char_check += letter
+        if char_check == checksum:
+            passing_element.append([encrypt, sector])
+            total += sector
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    for element, sector in passing_element:
+        string = ''
+        for character in element:
+            if character == '-':
+                string += ' '
+            else:
+                string += alphabet[(alphabet.index(character) + sector) % 26]
+        if 'pole' in string:
+            print(sector)
+            break
 
 part_one()
 part_two()
