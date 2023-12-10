6e213ed0a2f2ec7e576c6622a5c4f815cdeed405
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-11 00:17:08 +0100

    ➕ Add day 6 for 2016

--- 2016/december06/script.py
+++ 2016/december06/script.py
@@ -1,15 +1,33 @@
-import re
+from collections import Counter
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
     lines = file.read().splitlines()
 
 
+        
+
 def part_one():
-    pass
+    column = []
+    str_column = ''
+    str_corrected = ''
+    for i in range(len(lines[0])):
+        for j in range(len(lines)):
+            str_column += lines[j][i]
+        str_corrected += Counter(str_column).most_common()[0][0][0]
+        str_column = ''
+    print(str_corrected)
 
 
 def part_two():
-    pass
+    column = []
+    str_column = ''
+    str_corrected = ''
+    for i in range(len(lines[0])):
+        for j in range(len(lines)):
+            str_column += lines[j][i]
+        str_corrected += Counter(str_column).most_common()[-1][0][0]
+        str_column = ''
+    print(str_corrected)
 
 part_one()
 part_two()
