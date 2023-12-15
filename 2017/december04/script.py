3c7c141fdc4c8a348325d5a4fd235bbe01186e0f
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-16 00:00:36 +0100

    ➕ add day 4 for 2017

--- 2017/december04/script.py
+++ 2017/december04/script.py
@@ -1,15 +1,30 @@
-import re
-
 with open("./input.txt", 'r', encoding='utf-8') as file:
     lines = file.read().splitlines()
 
 
-def part_one():
-    pass
-
+def part_one(lines=lines):
+    total = 0
+    for line in lines:
+        line = line.split(" ")
+        total += len(set(line)) == len(line)
+    print(total)
 
 def part_two():
-    pass
+    total = 0
+    for line in lines:
+        line = line.split(" ")
+        if len(set(line)) != len(line):
+            continue
+        is_valid = True
+        for i in range(len(line)):
+            for j in range(i+1, len(line)):
+                if "".join(sorted(line[i])) == "".join(sorted(line[j])):
+                    is_valid = False
+                    break
+            if not is_valid:
+                break
+        total += is_valid
+    print(total)
 
 part_one()
 part_two()
