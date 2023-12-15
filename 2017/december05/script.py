9ac316c14c0a2634ed2df589f86575d3809d6e78
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-16 00:20:57 +0100

    ➕ Add day 5 for 2017 Bad delta time for part2 because i'm too dumb. I've been running part one before, causing instructions to change and make the result incorrect, even if the algorithm for part 2 was perfect

--- 2017/december05/script.py
+++ 2017/december05/script.py
@@ -1,15 +1,32 @@
 import re
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
-    lines = file.read().splitlines()
+    instructions = [int(re.findall(r"-?\d+",line)[0]) for line in file.read().splitlines()]
 
 
 def part_one():
-    pass
+    steps = 0
+    index = 0
+    while index >= 0 and index < len(instructions):
+        previous_index = index
+        index += instructions[index]
+        instructions[previous_index] += 1
+        steps += 1
+    print(steps)
 
 
 def part_two():
-    pass
+    index = 0
+    steps = 0
+    while 0 <= index < len(instructions):
+        value = instructions[index]
+        if value >= 3:
+            instructions[index] -= 1
+        else:
+            instructions[index] += 1
+        index += value
+        steps += 1
+    print(steps)
 
-part_one()
+#part_one()
 part_two()
