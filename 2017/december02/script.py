a86d8ef85e514f58d8018cc5b4d61054c3cc696c
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-15 23:19:47 +0100

    ➕ add day 2 for 2017

--- 2017/december02/script.py
+++ 2017/december02/script.py
@@ -1,15 +1,23 @@
 import re
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
-    lines = file.read().splitlines()
+    lines = [list(map(int, re.findall(r"\d+",line))) for line in file.read().splitlines()]
 
 
 def part_one():
-    pass
+    print(sum([max(numbers) - min(numbers) for numbers in lines]))
 
 
-def part_two():
-    pass
+def part_two(lines=lines):
+    total = 0
+    for numbers in lines:
+        for i in range(len(numbers)):
+            for j in range(len(numbers)):
+                if i == j:
+                    continue
+                if (numbers[i] % numbers[j]) == 0:
+                    total += int(numbers[i] / numbers[j])
+    print(total)
 
 part_one()
 part_two()
