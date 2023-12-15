cfc60ca96997c4fe0e6eeaff4d0eaddf41cf7d76
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-15 16:00:25 +0100

    ➕ add day 15 for 2023

--- 2023/december15/script.py
+++ 2023/december15/script.py
@@ -1,15 +1,41 @@
-import re
-
 with open("./input.txt", 'r', encoding='utf-8') as file:
-    lines = file.read().splitlines()
+    instructions = file.read().split(",")
+
+def hash(string):
+    current_value = 0
+    for character in string:
+        current_value += ord(character)
+        current_value *= 17
+        current_value %= 256
+    return current_value
 
 
 def part_one():
-    pass
+    print(sum(map(hash, instructions)))
 
 
 def part_two():
-    pass
+    boxes = [[] for _ in range(256)]
+    focals = {}
+    for instruc in instructions:
+        if "-" in instruc:
+            label = instruc[:-1]
+            box_index = hash(label)
+            if label in boxes[box_index]:
+                boxes[box_index].remove(label)
+        else:
+            label, length = instruc.split("=")
+            length = int(length)
+            box_index = hash(label)
+            if label not in boxes[box_index]:
+                boxes[box_index].append(label)
+            focals[label] = length
+    
+    total = 0
+    for box_index, box in enumerate(boxes, 1):
+        for focal_index, label in enumerate(box, 1):
+            total += box_index * focal_index * focals[label]
+    print(total)
 
 part_one()
 part_two()
