61fdc1609d12e0b14e964abc32654fe5096398c2
Ricozzi Romain <risingsunlightdev@gmail.com>
2023-12-25 11:10:47 +0100

    ➕ add day 25 for 2023 Merry Christmas, folks ! 🎄

--- 2023/december25/script.py
+++ 2023/december25/script.py
@@ -1,15 +1,27 @@
-import re
+# See to understand why this script works https://en.wikipedia.org/wiki/Girvan%E2%80%93Newman_algorithm
+from collections import defaultdict
+import networkx as nx
 
 with open("./input.txt", 'r', encoding='utf-8') as file:
     lines = file.read().splitlines()
 
 
 def part_one():
-    pass
+    graph = defaultdict(dict)
+
+    for line in lines:
+        src, destinations = line.split(': ')
+        for dest in destinations.split():
+            graph[src][dest] = {'weight': 1}
+        
+    computed_graph = nx.from_dict_of_dicts(graph)
+    result = next(nx.community.girvan_newman(computed_graph))
+    print(len(result[0]) * len(result[1]))
 
 
 def part_two():
-    pass
+    print("Merry Christmas ! :D")
+
 
 part_one()
 part_two()
